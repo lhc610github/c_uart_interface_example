09197d4445dca07877cad8c62a26294e2db44c28
lhc610github <604340918@qq.com>
2017-12-05 20:03:27 +0800

    test3

--- lcm_interface.cpp
+++ lcm_interface.cpp
@@ -334,7 +334,7 @@ void Lcm_Interface:: send_thread() {
     while( ! time_to_exit )
     {
         send_lcm_messages();
-        usleep(40000); // Send at 10Hz
+        usleep(48000); // Send at 10Hz
     }
 
     return;
